7a419a0645ef8d88666353e5b6744c59740da3f8
Lucho Guijo <ylucia.porral@gmail.com>
2022-10-04 10:07:06 -0300

    Added Promises and MockAPI

---
+++ src/components/Pet.jsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function PetCard(props) {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Adopt Me!
+        </Typography>
+        <Typography variant="h5" component="div">
+          {props.name}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {props.age}
+        </Typography>
+        <Typography variant="body2">
+          {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/components/PetListContainer.jsx
+++ src/components/PetListContainer.jsx
@@ -1,7 +1,31 @@
-import ItemCount from "./ItemCount"
+import React, {useState, useEffect} from "react";
+import ItemCount from "./ItemCount";
+import getPets from "../mockAPI";
+import PetCard from "./Pet";
 
 const PetListContainer = () =>{
+    let [data, setData] = useState([]);
+
+   useEffect(()=>{
+    console.log("UseEffect")
+        getPets().then((dataResponse) => {
+            setData(dataResponse)
+        })    
+    },
+    []
+   )
     return(
-        <div><ItemCount/></div>
+        <div>
+            {
+                data.map( (pet) => {
+                    return (
+                    <PetCard
+                            name={pet.name}
+                            description ={pet.description}
+                            age = {pet.age}
+                    />);}
+                )
+                }
+        </div>
     )
 };export default PetListContainer
