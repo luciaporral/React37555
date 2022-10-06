c919e8f7cf161f0f334f84f54a5c5437dce2ddbd
Lucho Guijo <ylucia.porral@gmail.com>
2022-10-06 07:38:57 -0300

    Se agregó el ruteo en la App y en el botón Ver Mas

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import NavBar from './components/NavBar';
 import * as React from 'react';
 import PetListContainer from './components/PetListContainer'
 import PetDetail from "./components/PetDetail"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 function App() {
   return (
 
-    <div className="App">
-        <NavBar/>
-        <PetDetail/>
-    </div>
+    <PetDetail/>
   );
 }
 
